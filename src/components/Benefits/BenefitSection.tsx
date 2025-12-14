12cfb408b2634c6ad92fba8d26b9fd8195c68403
proSamik <samikchoudhury15@gmail.com>
2025-12-14 09:32:31 +0530

    Refactor BenefitSection component to enforce type safety by explicitly defining Variants type for container and child variants.

--- src/components/Benefits/BenefitSection.tsx
+++ src/components/Benefits/BenefitSection.tsx
@@ -21,7 +21,7 @@ const containerVariants: Variants = {
         opacity: 1,
         y: 0,
         transition: {
-            type: "spring",
+            type: "spring" as const,
             bounce: 0.2,
             duration: 0.9,
             delayChildren: 0.2,
@@ -30,7 +30,7 @@ const containerVariants: Variants = {
     }
 };
 
-export const childVariants = {
+export const childVariants: Variants = {
     offscreen: {
         opacity: 0,
         x: -50,
@@ -39,7 +39,7 @@ export const childVariants = {
         opacity: 1,
         x: 0,
         transition: {
-            type: "spring",
+            type: "spring" as const,
             bounce: 0.2,
             duration: 1,
         }
