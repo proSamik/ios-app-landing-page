62c70720f73028f540038d0bf78d669f01a4c11b
proSamik <samikchoudhury15@gmail.com>
2025-12-14 10:15:20 +0530

    Update footer social media link for X to reflect branding changes.

--- src/data/footer.ts
+++ src/data/footer.ts
@@ -26,7 +26,7 @@ export const footerDetails: {
     telephone: '+1 (123) 456-7890',
     socials: {
         // github: 'https://github.com',
-        x: 'https://twitter.com/x',
+        x: 'https://x.com/',
         facebook: 'https://facebook.com',
         // youtube: 'https://youtube.com',
         linkedin: 'https://www.linkedin.com',
